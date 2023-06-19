6a1b8a7f8362544372f04c1283aca0ea4ec75ddd
bakmaiev <thisyoungrhyme@gmail.com>
2023-06-19 16:03:33 +0300

    Add new changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,12 @@ import { ContactsList } from './ContactsList';
 import { Filter } from './ContactsFilter';
 import { Title } from './Title';
 import { Main } from './App.styled';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectContacts } from 'redux/selectors';
+import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchContacts } from 'redux/operations';
 
 export const App = () => {
   const dispatch = useDispatch();
-  const { isLoading, error } = useSelector(selectContacts);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -20,7 +18,6 @@ export const App = () => {
     <Main>
       <PhonebookForm title="Phonebook" />
       <Title title="Contacts"></Title>
-      {isLoading && !error && <b>Request in progress...</b>}
       <Filter></Filter>
       <ContactsList title="Contacts" />
     </Main>
